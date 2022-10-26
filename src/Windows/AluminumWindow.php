099a4c507aa5e2fd8baeef20ddf7ee6c8f7eba27
mariusbij <mariusvolovnik@gmail.com>
2022-10-26 09:28:38 +0300

    abstract windows

--- src/Windows/AluminumWindow.php
+++ src/Windows/AluminumWindow.php
@@ -2,46 +2,12 @@
 
 namespace Marius\KiloWindowManufacturer\Windows;
 
-use Marius\KiloWindowManufacturer\Painters\Colors\ColorInterface;
-use Marius\KiloWindowManufacturer\Experts\WindowExpertInterface;
-
-class AluminumWindow implements WindowInterface
+class AluminumWindow extends Window implements WindowInterface
 {
     public const TYPE = 'aluminum';
 
-    private string $description;
-
-    private WindowExpertInterface $expert;
-
-    private ColorInterface $color;
-
     public function __construct()
     {
-        $this->description = sprintf('This is a %s window', self::TYPE);
-    }
-
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-
-    public function getExpertDescription(): string
-    {
-        return sprintf('I was installed by %s', $this->expert->getDescription());
-    }
-
-    public function getColorDescription(): string
-    {
-        return sprintf('I am %s', $this->color->getDescription());
-    }
-
-    public function setColor(ColorInterface $color): void
-    {
-        $this->color = $color;
-    }
-
-    public function setExpert(WindowExpertInterface $expert): void
-    {
-        $this->expert = $expert;
+        $this->setDescription(self::TYPE);
     }
 }
---
+++ src/Windows/Window.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Marius\KiloWindowManufacturer\Windows;
+
+use Marius\KiloWindowManufacturer\Experts\WindowExpertInterface;
+use Marius\KiloWindowManufacturer\Painters\Colors\ColorInterface;
+
+abstract class Window implements WindowInterface
+{
+    protected string $description;
+
+    protected WindowExpertInterface $expert;
+
+    protected ColorInterface $color;
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $string): void
+    {
+        $this->description = sprintf('This is a %s window', $string);
+    }
+
+    public function getExpertDescription(): string
+    {
+        return sprintf('I was installed by %s', $this->expert->getDescription());
+    }
+
+    public function getColorDescription(): string
+    {
+        return sprintf('I am %s', $this->color->getDescription());
+    }
+
+    public function setColor(ColorInterface $color): void
+    {
+        $this->color = $color;
+    }
+
+    public function setExpert(WindowExpertInterface $expert): void
+    {
+        $this->expert = $expert;
+    }
+}
